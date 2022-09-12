fdfe585b1db08d5d7a11fe8363232fed78ed62e7
Andrea Romero <andrearx52@gmail.com>
2022-09-12 14:39:14 -0300

    Sumando DOM y Eventos

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ alert ("Bienvenid@s a SonVegcetas");
 
 
 let cupo = 1;
-let totalcupos = 2;
+let totalcupos = 1;
 while (cupo <= totalcupos){
 
     let name = prompt("Ingrese su nombre");
@@ -20,15 +20,15 @@ while (cupo <= totalcupos){
             let saludo = `Hola ${name} tu cupo es el nro ${cupo} el importe de los cursos es de 2000 pesos (Incluye recetario y merienda con té y budines) y 2400 pesos (Incluye degustaciones de todo el menu + recetarios.)`;
             cupo++;
             alert (saludo);
-
+        }
 
         }
 
 
-}
+
 //Cálculo de pago//
 
-let precio, monto, resultado, opcionDePago, opcionValida=0;
+/*let precio, monto, resultado, opcionDePago, opcionValida=0;
 
 const calcular = (precio,opcion) => {
     resultado = precio*opcion;
@@ -81,7 +81,7 @@ while (opcionValida===0) {
 alert ("Los pagos se realizan el dia del curso en nuestras instalaciones. Muchas gracias por elegirnos, esperamos que lo disfrutes! nos vemos pronto.");
 
 
-//Funcion constructora- estudiantes inscritos en el curso//
+//Funcion constructora- estudiantes inscritos en el curso- objetos//
 
 function Students(name, alimentacion, edad){
     this.name = name;
@@ -95,6 +95,8 @@ const studentTwo = new Students("andrea", "veggie", 34);
 console.log(studentOne);
 console.log(studentTwo);
 
+//Arrays//
+/*
 alert("Sorpresa: Incorporamos libros a nuestra tienda!!")
 
 
@@ -126,4 +128,58 @@ alert("Sorpresa: Incorporamos libros a nuestra tienda!!")
     let mensaje = "Muchas Gracias, saludos.";
     alert(mensaje);
 
+    };*/
+
+//DOM//
+
+    let options = ["Quesos", "Dips", "Panes", "Sin Tacc", "Drinks"];
+    let lists = document.getElementById("options");
+           for(const option of options){
+             let li = document.createElement("li");
+             li.innerHTML = option;
+             lists.append(li);
+}
+
+//Eventos//
+
+    const option1 = document.querySelector(".option1");
+
+    const biggest = () =>{
+        option1.style.transform = "scale(1.3)";
+
     };
+
+    const smaller = () =>{
+        option1.style.transform = "scale(1.0)";
+
+    }
+
+    option1.addEventListener("mouseenter", biggest);
+
+    option1.addEventListener("mouseleave", smaller);
+
+
+    let newsegment = [];
+    let info = document.getElementById("info");
+    let boxs = document.getElementById("boxs");
+    let elections= "Quieres agregar: "
+
+    info.addEventListener("submit", (event) => {
+        event.preventDefault();
+        boxs.innerHTML = "";
+        let inputs = event.target.children;
+        newsegment.push(inputs[0].value);
+
+    newsegment.forEach(item => {
+        let div = document.createElement("div");
+        div.innerHTML = (elections + item);
+        boxs.append(div);
+  })
+
+})
+
+
+
+
+
+
