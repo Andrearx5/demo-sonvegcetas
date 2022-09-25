ae7b7934190aca5f8a5a61f5afcc0150601d58ff
Andrea Romero <andrearx52@gmail.com>
2022-09-25 16:06:40 -0300

    Agregando un carrito

--- index.js
+++ index.js
@@ -1,49 +1,65 @@
-//alert ("Bienvenid@s a SonVegcetas");
-//let name = prompt("Ingrese su nombre");
-//const namename = document.querySelector(".namealert").innerHTML = "BIENVENID@ A NUESTRO ESPACIO " + name; 
+//Carrito//
 
-//STORAGE//
+let carrito = []
 
-    let user;
-    let userStorage = sessionStorage.getItem("user");
-    if(userStorage){
-      let message = `Hola ${userStorage} gracias por volver!`;
-      alert(message);
-    }else{
-      user = prompt("Ingrese su nombre");
-      sessionStorage.setItem("user", user);
-      alert("Bienvenid@ a SonVegcetas");
-    } 
+let list_books = [
+    { id: 1, nombre: "Hola Vegan", cantidad: 3, precio: 5500, img: "./Imagenes/Libro hola vegan.webp"},
+    { id: 2, nombre: "Comida real", cantidad: 5, precio: 2880, img: "./Imagenes/Libro comida real.webp"},
+    { id: 3, nombre: "Cocina vegetariana", cantidad: 4, precio: 2400, img: "./Imagenes/Cocina vegetariana.webp"},
+    { id: 4, nombre: "Vegan Gourmet", cantidad: 5, precio: 2900, img: "./Imagenes/Vegan gourmet.webp"},
+    { id: 4, nombre: "La cocina de Daksha", cantidad: 3, precio: 5040, img: "./Imagenes/La cocina de Daksha.webp"},
+  ];
 
-//JSON//
-
-    let books = [
-         { id: 1, nombre: "Cocina facil", precio: 1000 },
-         { id: 2, nombre: "Como comer mejor", precio: 1100 },
-         { id: 3, nombre: "Recetarios veganos", precio: 500 },
-         { id: 4, nombre: "La nueva salud", precio: 700 },
-    ];
-
-    localStorage.setItem("books", JSON.stringify(books));
+const listbookbox = document.getElementById("bookbox");
 
 
-//DOM//
+list_books.forEach((books) => {
+    const div = document.createElement ("div");
+    div.classlist.add('books');
+    div.innerHTML = ` 
+    <img src=${books.img} alt ="">
+    <h3>${books.nombre}</h3>
+    <p class = "prices"> Precio: $ ${books.precio}</p>
+    <button id = "Agregar ${books.id}" class = "button-add"> Agregar <i class fas fa-shopping-cart"></i></buttton>
+    `
 
-    let options = ["Quesos", "Dips", "Panes", "Tartas", "Muffins", "Guisos", "Sin Tacc", "Drinks"];
+    bookbox.appendChild(div);
+
+    });
+
+
+
+
+//DOM// SessionStorage // 
+
+/*
+
+    let form = document.getElementById("forms");
+    form.onclick = function(e){
+        e.preventDefault();
+        let name = document.getElementById("name").value;
+        name != "" ? document.querySelector(".namealert").innerHTML = "BIENVENID@ A NUESTRO ESPACIO " + name : document.querySelector(".namealert").innerHTML = "PARA UNA MEJOR EXPERIENCIA INGRESE SU NOMBRE";
+
+        sessionStorage.setItem("name", name);
+
+}
+
+  /*  let options = ["Quesos", "Dips", "Panes", "Tartas", "Muffins", "Guisos", "Sin Tacc", "Drinks"];
     let lists = document.getElementById("options");
            for(const option of options){
              let li = document.createElement("li");
              li.innerHTML = option;
              lists.append(li);
             }
+         
 
-
-//Eventos//
+//Eventos// LocalStorage // 
 
     let newsegment = [];
     let info = document.getElementById("info");
     let boxs = document.getElementById("boxs");
     let elections= "Quieres agregar: "
+
 
     info.addEventListener("submit", (event) => {
         event.preventDefault();
@@ -56,38 +72,17 @@
         div.innerHTML = (elections + item);
         boxs.append(div);
   })
+    
+    localStorage.setItem("newsegment", JSON.stringify(newsegment)); 
 
-})
-
-
-/*const option1 = document.querySelector(".option1");
-    const option2 = document.querySelector(".option2");
-    const option3 = document.querySelector(".option3");
-
-    const biggest = () =>{
-        option1.style.transform = "scale(1.3)";
-        option2.style.transform = "scale(1.4)";
-        option3.style.transform = "scale(1.2)";
-    };
-
-    const smaller = () =>{
-        option1.style.transform = "scale(1.0)";
-        option2.style.transform = "scale(1.0)";
-        option3.style.transform = "scale(1.0)"
-
-    }
-
-    /*option1.addEventListener("mouseenter", biggest);
-    option2.addEventListener("mouseenter", biggest);
-    option3.addEventListener("mouseenter", biggest);
-
-    option1.addEventListener("mouseleave", smaller);
-    option2.addEventListener("mouseleave", smaller);
-    option3.addEventListener("mouseleave", smaller);*/
+    //let totalElections = [];
+    let totalElections = localStorage.getItem("newsegment");
+    
 
 
+  
 
+});
 
-
-
-
+ 
+*/
